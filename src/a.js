c83132b85afac7215d056e43e9409950908c95c3
Dario Piotrowicz <dario@cloudflare.com>
2023-09-24 22:13:50 +0100

    resolve to local a.js

---
+++ src/a.js
@@ -0,0 +1,3 @@
+export function fnA(str) {
+    return str.toUpperCase();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,38 @@ module.exports = {
   mode: 'development',
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            exclude: /nodeModules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: [ "@babel/preset-env" ],
-                },
-            },
+      {
+        test: /\.js$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+    plugins: [
+      {
+        apply: (compiler) => {
+          compiler.hooks.compilation.tap(
+            "AnySchemeUriPlugin",
+            (_compilation, { normalModuleFactory }) => {
+              normalModuleFactory.hooks.resolve
+                .tap("AnySchemeUriPlugin", resourceData => {
+                  if(resourceData.request === 'custom:module-a') {
+                    resourceData.request = './a.js';
+                  }
+                });
+            }
+          );
+        },
+      }
+    ]
+  
 };
